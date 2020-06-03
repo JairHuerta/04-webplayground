c5d626ffcc00a366276c3fdfec0254fc4388401c
Jair Ramón Huerta <jair.17huerta@gmail.com>
2020-06-03 00:54:16 -0500

    validación de email unico

--- webplayground/registration/form.py
+++ webplayground/registration/form.py
@@ -8,3 +8,8 @@ class UserCreationFormWithEmail(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+    
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("El email ya esta siendo usado, intenta con otro")
