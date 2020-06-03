769ecfb0cc48307ca40daa2c08c2054f84a339aa
Jair Ramón Huerta <jair.17huerta@gmail.com>
2020-06-03 01:37:24 -0500

    creando perfil de usuario

--- webplayground/webplayground/settings.py
+++ webplayground/webplayground/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-LOGIN_REDIRECT_URL = 'pages:pages'
+# LOGIN_REDIRECT_URL = 'pages:pages'
 LOGOUT_REDIRECT_URL = 'home'
 
 
@@ -136,3 +136,8 @@ if DEBUG:
 else:
     #Aquóe hay que configurareun email real para producción
     pass
+
+#Media Files
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
