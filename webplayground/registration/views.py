f308ec112d0c0e804cfef4f711fe283e78105c3a
Jair Ramón Huerta <jair.17huerta@gmail.com>
2020-06-03 02:31:40 -0500

    mejorando el formulario de perfil

--- webplayground/registration/views.py
+++ webplayground/registration/views.py
@@ -1,4 +1,4 @@
-from .form import UserCreationFormWithEmail, ProfileForm
+from .form import UserCreationFormWithEmail, ProfileForm, EmailForm
 from django.views.generic import CreateView
 from django.views.generic.edit import UpdateView
 from django.utils.decorators import method_decorator
@@ -35,3 +35,19 @@ class ProfileUpdate(UpdateView):
         # recuperar el objeto que se va a editar
         profile, created = Profile.objects.get_or_create(user=self.request.user)
         return profile
+
+@method_decorator(login_required, name='dispatch')
+class EmailUpdate(UpdateView):
+    form_class = EmailForm
+    success_url = reverse_lazy('profile')
+    template_name = 'registration/profile_email_form.html'
+
+    def get_object(self):
+        # recuperar el objeto que se va a editar
+        return self.request.user
+    
+    def get_form(self, form_class=None):
+        form = super(EmailUpdate, self).get_form()
+        #Modificar en tiempo real
+        form.fields['email'].widget = forms.EmailInput(attrs={'class':'form-control mb-2', 'placeholder':'Nombre de usuario'})
+        return form    
