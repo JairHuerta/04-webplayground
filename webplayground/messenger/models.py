74f10f6305e9f3bbfd86fc52d7fa463c9275278e
Jair Ramón Huerta <jair.17huerta@gmail.com>
2020-06-05 20:53:31 -0500

    creando el Model Manager

--- webplayground/messenger/models.py
+++ webplayground/messenger/models.py
@@ -11,9 +11,27 @@ class Message(models.Model):
     class Meta:
         ordering = ['created']
 
+
+class ThreadManager(models.Manager):
+    def find(self, user1, user2):
+        queryset = self.filter(users=user1).filter(users=user2)
+        if len(queryset) > 0:
+            return queryset[0]
+        return None
+    
+    def find_or_create(self, user1, user2):
+        thread = self.find(user1, user2)
+        if thread is None:
+            thread = Thread.objects.create()
+            thread.users.add(user1, user2)
+        return thread
+
 class Thread(models.Model):
     users = models.ManyToManyField(User, related_name='threads')
     messages = models.ManyToManyField(Message)
+
+    objects = ThreadManager()
+
 
 def messages_changed(sender, **kwargs):
     instance = kwargs.pop("", None)
@@ -28,5 +46,8 @@ def messages_changed(sender, **kwargs):
             if msg.user not in instance.users.all():
                 print("Ups, {{}} no forma parte del hilo".format(msg.user))
                 false_pk_set.add(msg_pk)
+
+    #Buscar los mensajes de false_pk_set que si están en pk_set y los borramos del pk_set
+    pk_set.difference_update(false_pk_set)
 
 m2m_changed.connect(messages_changed, sender=Thread.messages.through)
